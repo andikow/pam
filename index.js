101dcb6a7b4956365ba2e6fb976e8782d9877d8f
andikow <katama_andi@yahoo.com>
2021-04-13 20:45:20 +0700

    Fix Error Dependencies

--- index.js
+++ index.js
@@ -4,7 +4,19 @@
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 import { Container, Header, Button, StyleProvider } from 'native-base';
-import CetakLabelKirim from './screen/CetakLabelKirim';
+import BuatAkunBaru from './screen/BuatAkunBaru';
+import CetakLabelKirim from './screen/CetakLabelKirim.js';
+import DetailTransaksi from './screen/detailtransaksi.js';
+import Login from './screen/Login.js';
+import Mulai from './screen/Mulai.js';
+import Navigasi from './screen/Navigasi.js';
+import OpsiEkspedisi from './screen/OpsiEkspedisi.js';
+import OpsiLayanan from './screen/OpsiLayanan.js';
+import Password from './screen/Password.js';
+import PasswordBaru from './screen/PasswordBaru.js';
+import StatTransaksi from './screen/statTransaksi.js';
+import Verifikasi from './screen/Verifikasi.js';
+import AppNavigator from './Pengaturan/RootPengaturan.js';
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material.js';
 import {name as appName} from './app.json';
@@ -21,4 +33,4 @@ export default class App extends Component {
     );
   }
 }
-AppRegistry.registerComponent(appName, () => CetakLabelKirim);
+AppRegistry.registerComponent(appName, () => AppNavigator);
